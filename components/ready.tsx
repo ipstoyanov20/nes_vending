c985d1d5112ad7756c4577e90cf778160636399c
ipstoyanov20 <ipstoyanov20@codingburgas.bg>
2024-10-03 10:25:32 +0300

    Work on mobile

--- components/ready.tsx
+++ components/ready.tsx
@@ -4,7 +4,7 @@ function Ready() {
   return (
     <section className='realtive w-screen h-screen bg-[#0b182b]'>
         <div className='grid place-items-center '>
-            <h1 className='sm:text-8xl text-5xl text-white font-bold font-nuni text-center mt-5 p-10'>Готови да започнем следващото приключение?</h1>
+            <h1 className='sm:text-8xl text-4xl text-white font-bold font-nuni text-center mt-5 p-10'>Готови за следващото приключение?</h1>
             <HoverSwitch/>
         </div>
     </section>
